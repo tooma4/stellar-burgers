3a7ac4a1f00529f020720688c09b27c1a5561dc0
Roman Starshinov <padoshvars@yandex.ru>
2024-12-21 00:07:15 +0300

    The project work has been fully completed--

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,13 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-<<<<<<< HEAD
+
 import constructorSlice from './slices/constructorSlice/constructorSlice';
 import orderSlice from './slices/orderSlice/orderSlice';
 import feedSlice from './slices/feedSlice/feedSlice';
 import userSlice from './slices/userSlice/userSlice';
 import ingredientSlice from './slices/ingredientSlice/ingredientSlice';
-=======
-import { ingredientSlice } from './slices/ingredients/ingredientsSlice';
->>>>>>> 525f76303a7afb5efb513020cccef40536cc9d58
 
 import {
   TypedUseSelectorHook,
@@ -15,27 +12,19 @@ import {
   useSelector as selectorHook
 } from 'react-redux';
 
-<<<<<<< HEAD
 const rootReducer = combineReducers({
   ingredient: ingredientSlice,
   order: orderSlice,
   constructorBurger: constructorSlice,
   feed: feedSlice,
   user: userSlice
-=======
-export const rootReducer = combineReducers({
-  ingredient: ingredientSlice
->>>>>>> 525f76303a7afb5efb513020cccef40536cc9d58
 });
 
 const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production'
 });
-<<<<<<< HEAD
 
-=======
->>>>>>> 525f76303a7afb5efb513020cccef40536cc9d58
 export type RootState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
